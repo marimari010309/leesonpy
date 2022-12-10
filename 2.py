86e86b7310c920ddfd7c2cdf258395c97fd179cb
marimari010309 <120250520+marimari010309@users.noreply.github.com>
2022-12-10 16:05:04 +0500

    Delete 2.py

--- 2.py
+++
@@ -1,5 +0,0 @@
-Python 3.8.7 (tags/v3.8.7:6503f05, Dec 21 2020, 17:43:54) [MSC v.1928 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print('Раз','Два','Три?', sep='*')
-Раз*Два*Три?
->>> 
